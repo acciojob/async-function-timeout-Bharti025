baf35fe692ded3b8ced3073e28921133510b2631
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-08-22 13:37:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let inputText=document.getElementById("text");
+let inputNumber=document.getElementById("delay");
+const btn=document.getElementById("btn");
+let output=document.getElementById("output");
+
+function display(input1,input2){
+ return new Promise(resolve=>{
+	 setTimeout(()=>{
+		 resolve(`${input1} - ${input2}`);
+	 },input*1000);
+ })	
+}
+
+async function check(){
+let input1=inputText.value;
+let input2=parseInt(inputNumber.value);
+let data= await display(input1,input2);
+output.innerText=data;
+}
+
+btn.addEventListener("click",check);
