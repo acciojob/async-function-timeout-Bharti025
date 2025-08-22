e4a317ac8070c8e265fcd6a82169b5c74751162e
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-08-22 13:52:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,14 +9,26 @@ function display(input1,input2){
 	 setTimeout(()=>{
 		 resolve(input1);
 	 },input2*1000);
- })	
+ });	
 }
 
 async function check(){
 let input1=inputText.value;
 let input2=parseInt(inputNumber.value);
-let data= await display(input1,input2);
-output.innerText=data + " - " + input2;
+if (isNaN(input2) || input2 < 0) {
+        output.innerText = "Please enter a valid delay time.";
+        return;
+    }
+
+    try {
+        let data = await display(input1, input2);
+        output.innerText = data;
+    } catch (error) {
+        console.error("Error:", error);
+        output.innerText = "An error occurred.";
+    }
+}
+
 }
 
 btn.addEventListener("click",check);
