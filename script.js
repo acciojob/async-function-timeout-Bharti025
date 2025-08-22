7fb29ca5aa597d7c47d75fb6b580742de5a6e81f
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-08-22 13:40:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let output=document.getElementById("output");
 function display(input1,input2){
  return new Promise(resolve=>{
 	 setTimeout(()=>{
-		 resolve(`${input1} - ${input2}`);
+		 resolve("");
 	 },input*1000);
  })	
 }
@@ -16,7 +16,7 @@ async function check(){
 let input1=inputText.value;
 let input2=parseInt(inputNumber.value);
 let data= await display(input1,input2);
-output.innerText=data;
+output.innerText=`${input1} - ${input2}`;
 }
 
 btn.addEventListener("click",check);
